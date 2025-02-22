2053b436e38bb8b949915d09a5dd650659a98649
leonardoserra <serra.leonardo92@gmail.com>
2025-02-22 18:35:07 +0100

    added check for ordered iterable and if elements are numeric or not

--- linear_search.py
+++ linear_search.py
@@ -1,20 +1,36 @@
 import sys
 import argparse
 
-
-def linear_search(iterable: list | dict | str, search_el: object) -> int | None:
+def linear_search(iterable: list | dict | str, search_el: object, is_ordered=False) -> int | None:
     """
     :param iterable: where you wanna find the value
     :param type: list
     :param search_value: what you wanna find
     :param type: object
+    :param is_ordered: if the iterable is ordered set it to true
+    :param type: bool
     :return: Index or None
     :return type: int | None
     """
+    are_numbers = True
+
+    for e in iterable:
+        if not isinstance(e, int|float):
+            are_numbers = False
+
+    if is_ordered and are_numbers:
+
+        for i, el in enumerate(iterable):
+            if el == search_el:
+                return i
+            elif el > search_el:
+                break
     
-    for i, el in enumerate(iterable):
-        if el == search_el:
-            return i
+    else:
+
+        for i, el in enumerate(iterable):
+            if el == search_el:
+                return i
 
     return None
 
@@ -27,6 +43,7 @@ def cli():
 
     parser.add_argument("-a", "--array")
     parser.add_argument("-sv", "--search-value")
+    parser.add_argument("--is-ordered", action='store_true', required=False, default=False)
 
     args = parser.parse_args()
 
@@ -39,8 +56,9 @@ def main(args):
 
         array = args.array.split(" ")
         search_el = args.search_value
+        is_ordered = args.is_ordered
 
-        i = linear_search(array, search_el)
+        i = linear_search(array, search_el, is_ordered)
 
         print(i)
 
